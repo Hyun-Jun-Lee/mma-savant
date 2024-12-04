7746beb5b891021d6c3b4fa7cdfa0a110c97ae05
Lee Hyun Jun <bhk0827@gmail.com>
2024-12-04 21:55:45 +0900

    feat

--- src/usecase/fighter_statics.py
+++ src/usecase/fighter_statics.py
@@ -1,21 +1,5 @@
 from exceptions.fighter_exception import FighterNotFoundException
 
-
-async def get_fighter_punch_accuracy(match_service, fighter_service, fighter_id: int):
-    """Fighter의 승리 및 패배 경기 펀치 성공률 반환"""
-
-    fighter = await fighter_service.get_fighter(fighter_id)
-    if not fighter:
-        raise FighterNotFoundException(fighter_id)
-    
-    win_accuracy = await match_service.get_punch_accuracy_by_result(fighter_id, "win")
-    loss_accuracy = await match_service.get_punch_accuracy_by_result(fighter_id, "loss")
-    
-    return {
-        "win_accuracy": win_accuracy,
-        "loss_accuracy": loss_accuracy
-    }
-
 async def get_fighter_with_statics(match_service, fighter_service, fighter_id: int):
     fighter = await fighter_service.get_fighter_by_id(fighter_id)
 
