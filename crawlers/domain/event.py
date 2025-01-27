a7a387c4d03649a67087dbb74f8fd915099dbdab
Lee Hyun Jun <bhk0827@gmail.com>
2025-01-27 12:15:27 +0900

    fix : get method

--- crawlers/domain/event.py
+++ crawlers/domain/event.py
@@ -7,11 +7,13 @@ class Event:
     name: str
     location: str
     date: date
+    url : str
 
     @classmethod
     def from_dict(cls, data: Dict):
         return cls(
-            name=data["name"],
-            location=data["location"],
-            date=data["date"],
+            name=data.get("name"),
+            location=data.get("location"),
+            date=data.get("date"),
+            url=data.get("url"),
         )
