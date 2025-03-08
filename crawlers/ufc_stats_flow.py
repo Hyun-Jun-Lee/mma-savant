7ba03df66149ba7d600c73364be01a527cda15b7
Lee Hyun Jun <bhk0827@gmail.com>
2025-03-09 01:36:56 +0900

    WIP

--- crawlers/ufc_stats_flow.py
+++ crawlers/ufc_stats_flow.py
@@ -1,6 +1,8 @@
 from typing import List
 
-from prefect import flow, task
+from prefect import Flow, task
+
+from repository import BaseRepository, FighterRepository, EventRepository
 
 @task
 def scrap_all_fighter():
@@ -15,18 +17,21 @@ def scrap_event_detail():
     pass
 
 @task
-def save_data(data : List[dict]):
+def save_data(data : List[dict], repository: BaseRepository):
     pass
 
 with Flow("ufc_stats_flow") as flow:
+    # TODO
+    session = None
+
     fighters_data = scrap_all_fighter()
-    save_data(fighters_data)
+    save_data(fighters_data, FighterRepository(session))
 
     # NOTE : query fighter id by dict[name, id]?
     events_data = scrap_all_events()
-    save_data(events_data)
+    save_data(events_data, EventRepository(session))
 
     # NOTE : query event id by dict[name, id]?
     event_detail_data = scrap_event_detail()
-    save_data(event_detail_data)
+    save_data(event_detail_data, EventRepository(session))
     
