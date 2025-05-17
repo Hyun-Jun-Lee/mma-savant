da78ac445a75a305f86debb618b8c9c3bbef5571
Lee Hyun Jun <bhk0827@gmail.com>
2025-05-17 21:58:35 +0900

    wip : Add test for workflow

---
+++ crawlers/workflows/tests/test_ufc_stats_flow.py
@@ -0,0 +1,51 @@
+import asyncio
+import time
+from typing import List, Dict
+
+from database.session import db_session
+from workflows.tasks import (
+    scrap_all_events_task,
+    scrap_all_fighter_task,
+    scrap_event_detail_task,
+    scrap_match_detail_task,
+)
+
+
+async def run_ufc_stats_flow():
+    print("UFC 통계 크롤링 시작")
+    start_time = time.time()
+    
+    # 이벤트 크롤링
+    with db_session() as session:
+        print("Events scraping started")
+        events_list = await scrap_all_events_task(session)
+        print(f"Events scraping completed : {len(events_list)} events saved")
+
+    # 파이터 크롤링
+    with db_session() as session:
+        print("Fighters scraping started")
+        fighter_list = await scrap_all_fighter_task(session)
+        print(f"Fighters scraping completed : {len(fighter_list)} fighters saved")
+
+    # 파이터 딕셔너리 생성
+    fighter_dict = {fighter.name: fighter.id for fighter in fighter_list}
+
+    # 이벤트 세부 정보 크롤링
+    with db_session() as session:
+        print("Event details scraping started")
+        fighter_match_dict = await scrap_event_detail_task(session, events_list, fighter_dict)
+        print(f"Event details scraping completed : {len(fighter_match_dict)} event details saved")
+
+    # 매치 세부 정보 크롤링
+    with db_session() as session:
+        print("Match details scraping started")
+        await scrap_match_detail_task(session, fighter_match_dict, fighter_dict)
+        print("Match details scraping completed")
+    
+    print("UFC 통계 크롤링 완료")
+    end_time = time.time()
+    print(f"Total time taken: {end_time - start_time:.2f} seconds")
+
+if __name__ == "__main__":
+    # 비동기 이벤트 루프에서 메인 함수 실행
+    asyncio.run(run_ufc_stats_flow())
