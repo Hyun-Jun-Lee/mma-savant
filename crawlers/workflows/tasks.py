8ab1d69e1097fc6df9e8d2ce76dd70224256ee0d
Lee Hyun Jun <bhk0827@gmail.com>
2025-05-31 22:58:31 +0900

    refactor : fix logging message

--- crawlers/workflows/tasks.py
+++ crawlers/workflows/tasks.py
@@ -12,60 +12,62 @@ from schemas import BaseSchema, Event, Fighter, Match, FighterMatch
 from scrapers import scrap_fighters, scrap_all_events, scrap_event_detail, scrape_match_basic_statistics, scrape_match_significant_strikes
 
 random_delay = random.randint(1, 5)
+logger = get_run_logger()
+
 
 def save_data(data : List[BaseSchema], repository: BaseRepository) -> List[BaseSchema]:
     return repository.bulk_upsert(data)
 
 @task(retries=3, cache_policy=NO_CACHE)
-async def scrap_all_fighter_task(session)-> List[Fighter]:
-    fighter_data = []
-    logger = get_run_logger()
+async def scrap_all_fighter_task(session)-> None:
+    logger.info("scrap_all_fighter_task started")
     for char in 'abcdefghijklmnopqrstuvwxyz':
         await asyncio.sleep(random_delay)
-        logger.info(f"[{char}] 크롤링 시작")
+        logger.info(f"[{char}] fighters scraping started")
         fighters_url = f"http://ufcstats.com/statistics/fighters?char={char}&page=all"
         try:
             fighter_schema_list = await scrap_fighters(fighters_url)
             saved_fighter_list = save_data(fighter_schema_list, FighterRepository(session))
-            fighter_data.extend(saved_fighter_list)
-            logger.info(f"[{char}] 크롤링 완료 : {len(saved_fighter_list)} fighters saved")
+            logger.info(f"[{char}] fighters scraping completed : {len(saved_fighter_list)} fighters saved")
         except Exception as e:
-            logger.error(f"[{char}] 크롤링 중 오류 발생: {str(e)}")
+            logger.error(f"[{char}] fighters scraping failed: {str(e)}")
             logger.error(format_exc())
-    return fighter_data
+
+    logger.info("scrap_all_fighter_task completed")
 
 
 @task(retries=3, cache_policy=NO_CACHE)
-async def scrap_all_events_task(session) -> List[Event]:
+async def scrap_all_events_task(session) -> None:
     all_events_url = "http://ufcstats.com/statistics/events/completed?page=all"
-    logger = get_run_logger()
     try:
+        logger.info("scrap_all_events_task started")
         event_schema_list = await scrap_all_events(all_events_url)
         saved_event_list = save_data(event_schema_list, EventRepository(session))
-        return saved_event_list
+        logger.info(f"scrap_all_events_task completed : {len(saved_event_list)} events saved")
     except Exception as e:
-        logger.error(f"크롤링 중 오류 발생: {str(e)}")
+        logger.error(f"scrap_all_events_task failed: {str(e)}")
         logger.error(format_exc())
 
 @task(retries=3, cache_policy=NO_CACHE)
-async def scrap_event_detail_task(session) -> Dict[str, Dict[int, FighterMatch]]:
-    logger = get_run_logger()
+async def scrap_event_detail_task(session) -> None:
+    logger.info("scrap_event_detail_task started")
     events_list = EventRepository(session).find_all()
     all_fighters = FighterRepository(session).find_all()
     fighter_dict = {fighter.name: fighter.id for fighter in all_fighters}
     
     for idx, event in enumerate(events_list):
         await asyncio.sleep(random_delay)
-        logger.info(f"[{idx+1}/{len(events_list)}] - event_id: {event.id} 이벤트 세부 정보 크롤링 중...")
+        logger.info(f"[{idx+1}/{len(events_list)}] - event_id: {event.id} event detail scraping started")
         event_url = event.url
         event_id = event.id
         try:
             matches_data = await scrap_event_detail(event_url, event_id, fighter_dict)
         except Exception as e:
-            logger.error(f"[{idx+1}/{len(events_list)}] - event_id: {event_id} 이벤트 세부 정보 크롤링 중 오류 발생: {str(e)}")
+            logger.error(f"[{idx+1}/{len(events_list)}] - event_id: {event_id} event detail scraping failed: {str(e)}")
             logger.error(format_exc())
             continue
 
+        saved_match_count = 0
         for match_data in matches_data:
             match = match_data["match"]
             saved_match = save_data([match], MatchRepository(session))
@@ -79,7 +81,9 @@ async def scrap_event_detail_task(session) -> Dict[str, Dict[int, FighterMatch]]
                 result = fighter_info["result"]
                 fighter_match = FighterMatch(fighter_id=fighter_id, match_id=match_id, result=result)
                 save_data([fighter_match], FighterMatchRepository(session))
-        logger.info(f"[{idx+1}/{len(events_list)}] - event_id: {event_id} 이벤트 세부 정보 크롤링 완료")
+            saved_match_count += len(saved_match)
+        logger.info(f"[{idx+1}/{len(events_list)}] - event_id: {event_id} event detail scraping completed : {saved_match_count} matches saved")
+    logger.info("scrap_event_detail_task completed")
 
 @task(retries=3, cache_policy=NO_CACHE)
 async def scrap_match_detail_task(session)-> None:
@@ -91,37 +95,34 @@ async def scrap_match_detail_task(session)-> None:
         fighter_match_dict: {detail_url: {fighter_id: fighter_match}} 형태의 딕셔너리
         fighter_dict: {fighter_name: fighter_id} 형태의 딕셔너리
     """
-    logger = get_run_logger()
+    logger.info("scrap_match_detail_task started")
     all_fighters = FighterRepository(session).find_all()
     fighter_dict = {fighter.name: fighter.id for fighter in all_fighters}
     fighter_match_dict = FighterMatchRepository(session).find_match_fighter_mapping()
 
     for idx, (detail_url, fighter_matches) in enumerate(fighter_match_dict.items()):
-        # TODO : need to delete
-        if idx < 200:
-            continue
         await asyncio.sleep(random_delay)
-        logger.info(f"[{idx+1}/{len(fighter_match_dict)}] - detail_url: {detail_url} 매치 세부 정보 크롤링 중...")
+        logger.info(f"[{idx+1}/{len(fighter_match_dict)}] - detail_url: {detail_url} match detail scraping started")
         if not detail_url:
             continue
             
         # 매치 기본 통계 정보 스크랩 및 저장
-        logger.info(f"[{idx+1}/{len(fighter_match_dict)}] - detail_url: {detail_url} BasicMatchStat 크롤링 중...")
         try:
             match_statistics_list = await scrape_match_basic_statistics(detail_url, fighter_dict, fighter_matches)
             save_data(match_statistics_list, BasicMatchStatRepository(session))
+            logger.info(f"[{idx+1}/{len(fighter_match_dict)}] - detail_url: {detail_url} BasicMatchStat scraping completed")
         except Exception as e:
-            logger.error(f"[{idx+1}/{len(fighter_match_dict)}] - detail_url: {detail_url} BasicMatchStat 크롤링 중 오류 발생: {str(e)}")
+            logger.error(f"[{idx+1}/{len(fighter_match_dict)}] - detail_url: {detail_url} BasicMatchStat scraping failed: {str(e)}")
             logger.error(format_exc())
             continue
         
         # 매치 스트라이크 상세 정보 스크랩 및 저장
-        logger.info(f"[{idx+1}/{len(fighter_match_dict)}] - detail_url: {detail_url} SigStrMatchStat 크롤링 중...")
         try:
             strike_details_list = await scrape_match_significant_strikes(detail_url, fighter_dict, fighter_matches)
             save_data(strike_details_list, SigStrMatchStatRepository(session))
+            logger.info(f"[{idx+1}/{len(fighter_match_dict)}] - detail_url: {detail_url} SigStrMatchStat scraping completed")
         except Exception as e:
-            logger.error(f"[{idx+1}/{len(fighter_match_dict)}] - detail_url: {detail_url} SigStrMatchStat 크롤링 중 오류 발생: {str(e)}")
+            logger.error(f"[{idx+1}/{len(fighter_match_dict)}] - detail_url: {detail_url} SigStrMatchStat scraping failed: {str(e)}")
             logger.error(format_exc())
             continue
-        logger.info(f"[{idx+1}/{len(fighter_match_dict)}] - detail_url: {detail_url} 매치 세부 정보 크롤링 완료")
+        logger.info(f"[{idx+1}/{len(fighter_match_dict)}] - detail_url: {detail_url} match detail scraping completed")
--- crawlers/workflows/ufc_stats_flow.py
+++ crawlers/workflows/ufc_stats_flow.py
@@ -1,39 +1,32 @@
-import logging
-
 from prefect import flow
-from prefect.schedules import Cron
-
+from prefect.logging import get_run_logger
 
 from database.session import db_session
 from workflows.tasks import scrap_all_fighter_task, scrap_all_events_task, scrap_event_detail_task, scrap_match_detail_task
 
-logger = logging.getLogger(__name__)
+logger = get_run_logger()
 
 @flow(log_prints=True)
 async def run_ufc_stats_flow():
-    logger.info("UFC 통계 크롤링 시작")
+    logger.info("======================")
+    logger.info("Start UFC stats scraping")
+    logger.info("======================")
     with db_session() as session:
-        logger.info("Events scraping started")
-        events_list = await scrap_all_events_task(session)
-        logger.info(f"Events scraping completed : {len(events_list)} events saved")
+        await scrap_all_events_task(session)
 
     # scrape fighters
     with db_session() as session:
-        logger.info("Fighters scraping started")
-        fighter_list = await scrap_all_fighter_task(session)
-        logger.info(f"Fighters scraping completed : {len(fighter_list)} fighters saved")
+        await scrap_all_fighter_task(session)
 
     # scrape event details
     with db_session() as session:
-        logger.info("Event details scraping started")
-        fighter_match_dict = await scrap_event_detail_task(session)
-        logger.info(f"Event details scraping completed : {len(fighter_match_dict)} event details saved")
+        await scrap_event_detail_task(session)
 
     # scrape match details
     with db_session() as session:
-        logger.info("Match details scraping started")
         await scrap_match_detail_task(session)
-        logger.info("Match details scraping completed")
-    logger.info("UFC 통계 크롤링 완료")
+    logger.info("======================")
+    logger.info("UFC stats scraping completed")
+    logger.info("======================")
 
 
