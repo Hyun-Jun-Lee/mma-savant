78dda4e9601fddc9727da149cccd2edb7b247c55
Lee Hyun Jun <bhk0827@gmail.com>
2025-06-01 04:42:58 +0900

    wip

--- crawlers/workflows/tests/test_ufc_stats_flow.py
+++ crawlers/workflows/tests/test_ufc_stats_flow.py
@@ -1,6 +1,7 @@
 import asyncio
 import time
-from typing import List, Dict
+from typing import Callable
+import logging
 
 from database.session import db_session
 from workflows.tasks import (
@@ -10,40 +11,43 @@ from workflows.tasks import (
     scrap_match_detail_task,
 )
 
+LOGGER = logging.getLogger(__name__)
 
-async def run_ufc_stats_flow():
-    print("UFC 통계 크롤링 시작")
+
+async def run_ufc_stats_flow(crawler_fn : Callable):
+    LOGGER.info("UFC 통계 크롤링 시작")
     start_time = time.time()
     
-    # 파이터 크롤링
-    with db_session() as session:
-        print("Fighters scraping started")
-        fighter_list = await scrap_all_fighter_task(session)
-        print(f"Fighters scraping completed : {len(fighter_list)} fighters saved")
+    # # # 파이터 크롤링
+    # with db_session() as session:
+    #     LOGGER.info("Fighters scraping started")
+    #     await scrap_all_fighter_task(session, crawler_fn)
+    #     LOGGER.info("Fighters scraping completed")
 
     # # 이벤트 크롤링
     # with db_session() as session:
-    #     print("Events scraping started")
-    #     events_list = await scrap_all_events_task(session)
-    #     print(f"Events scraping completed : {len(events_list)} events saved")
+    #     LOGGER.info("Events scraping started")
+    #     await scrap_all_events_task(session, crawler_fn)
+    #     LOGGER.info("Events scraping completed")
 
 
     # # 이벤트 세부 정보 크롤링
     # with db_session() as session:
-    #     print("Event details scraping started")
-    #     fighter_match_dict = await scrap_event_detail_task(session)
-    #     print(f"Event details scraping completed ")
+    #     LOGGER.info("Event details scraping started")
+    #     await scrap_event_detail_task(session, crawler_fn)
+    #     LOGGER.info("Event details scraping completed")
 
-    # # 매치 세부 정보 크롤링
-    # with db_session() as session:
-    #     print("Match details scraping started")
-    #     await scrap_match_detail_task(session)
-    #     print("Match details scraping completed")
+    # 매치 세부 정보 크롤링
+    with db_session() as session:
+        LOGGER.info("Match details scraping started")
+        await scrap_match_detail_task(session, crawler_fn)
+        LOGGER.info("Match details scraping completed")
     
-    # print("UFC 통계 크롤링 완료")
-    # end_time = time.time()
-    # print(f"Total time taken: {end_time - start_time:.2f} seconds")
+    LOGGER.info("UFC 통계 크롤링 완료")
+    end_time = time.time()
+    LOGGER.info(f"Total time taken: {end_time - start_time:.2f} seconds")
 
 if __name__ == "__main__":
+    from core.crawler import crawl_with_httpx
     # 비동기 이벤트 루프에서 메인 함수 실행
-    asyncio.run(run_ufc_stats_flow())
+    asyncio.run(run_ufc_stats_flow(crawl_with_httpx))
--- crawlers/workflows/ufc_stats_flow.py
+++ crawlers/workflows/ufc_stats_flow.py
@@ -2,6 +2,7 @@ from prefect import flow
 from prefect.logging import get_run_logger
 
 from database.session import db_session
+from core.crawler import crawl_with_httpx
 from workflows.tasks import scrap_all_fighter_task, scrap_all_events_task, scrap_event_detail_task, scrap_match_detail_task
 
 logger = get_run_logger()
@@ -12,19 +13,19 @@ async def run_ufc_stats_flow():
     logger.info("Start UFC stats scraping")
     logger.info("======================")
     with db_session() as session:
-        await scrap_all_events_task(session)
+        await scrap_all_events_task(session, crawl_with_httpx)
 
     # scrape fighters
     with db_session() as session:
-        await scrap_all_fighter_task(session)
+        await scrap_all_fighter_task(session, crawl_with_httpx)
 
     # scrape event details
     with db_session() as session:
-        await scrap_event_detail_task(session)
+        await scrap_event_detail_task(session, crawl_with_httpx)
 
     # scrape match details
     with db_session() as session:
-        await scrap_match_detail_task(session)
+        await scrap_match_detail_task(session, crawl_with_httpx)
     logger.info("======================")
     logger.info("UFC stats scraping completed")
     logger.info("======================")
