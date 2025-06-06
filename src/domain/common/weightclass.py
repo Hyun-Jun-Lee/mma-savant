b503f3ca2ab181d98d9f7e611b134ada30ca1ebd
Lee Hyun Jun <bhk0827@gmail.com>
2025-06-07 01:07:24 +0900

    remove : delete useless things...

--- src/domain/common/weightclass.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-from domain.common.base_entity import BaseEntity
-
-@dataclass
-class WeightClass(BaseEntity):
-    name : str
-    max_weight : float
-    min_weight : float
