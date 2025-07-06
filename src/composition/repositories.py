cac5094fd237e6d455af71856e6d1225591a9ec5
Lee Hyun Jun <bhk0827@gmail.com>
2025-07-06 23:52:24 +0900

    feat : Add composition repository method

--- src/composition/repositories.py
+++ src/composition/repositories.py
@@ -1,11 +1,13 @@
-from typing import List, Literal
+from typing import List, Literal, Optional
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import func, desc
+from datetime import date
 
 from fighter.models import FighterModel, FighterSchema
-from match.models import BasicMatchStatModel, FighterMatchModel, SigStrMatchStatModel
+from event.models import EventModel, EventSchema
+from match.models import BasicMatchStatModel, FighterMatchModel, MatchModel, SigStrMatchStatModel
 
 async def get_all_opponents(session: AsyncSession, fighter_id: int) -> List[FighterSchema]:
     subq = (
@@ -48,3 +50,21 @@ async def get_top_fighter_by_stat(session: AsyncSession, stat_model: Literal[Bas
     result = await session.execute(stmt)
     rows = result.fetchall()
     return [{"fighter": fighter.to_schema(), "stat": total_stat} for fighter, total_stat in rows]
+
+async def get_upcoming_fighter_match(session: AsyncSession, fighter_id: int) -> Optional[EventSchema]:
+    """
+    특정 파이터의 다가오는 경기(오늘 이후)가 포함된 가장 가까운 이벤트를 조회합니다.
+    """
+    result = await session.execute(
+        select(EventModel)
+        .join(EventModel.matches)
+        .join(MatchModel.fighter_matches)
+        .where(
+            FighterMatchModel.fighter_id == fighter_id,
+            EventModel.event_date > date.today()
+        )
+        .order_by(EventModel.event_date.asc())
+        .limit(1)
+    )
+    event = result.scalar_one_or_none()
+    return event.to_schema() if event else None
